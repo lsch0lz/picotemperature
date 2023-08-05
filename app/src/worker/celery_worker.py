815f91e83e9de737af2ee76a841b2f1a7a79ce18
Lukas Scholz <lscholz14@googlemail.com>
2023-08-05 09:14:23 +0200

    formatting

--- app/src/worker/celery_worker.py
+++ app/src/worker/celery_worker.py
@@ -8,6 +8,7 @@ redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 celery_app = Celery('worker', broker=redis_url, backend=redis_url)
 
+
 @celery_app.task
 def dummy_task():
     time.sleep(10)
@@ -16,3 +17,4 @@ def dummy_task():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(f"{folder}/task-{now}.txt", "w") as f:
         f.write("Celery task executed successfully")
+       
