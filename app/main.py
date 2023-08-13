f2ef36e5c3d3edc72cba919d7413a6b85d51b82d
Lukas Scholz <lscholz14@googlemail.com>
2023-08-13 10:41:48 +0200

    init

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from app.src.worker.prefect_task import repo_info
+
 app = FastAPI()
 
 
 @app.put("/temperature")
 def get_temperature():
     return {"Message": "200 OK"}
+
+@app.get("/start")
+def start_flow():
+    repo_info_val, contributors = repo_info()
+    return {"Name:": repo_info_val, "Contributors:": contributors}
