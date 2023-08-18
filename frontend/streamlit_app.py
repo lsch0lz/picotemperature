d8761a0fd62775ca23082534489e2aa3f6e438bf
Lukas Scholz <lscholz14@googlemail.com>
2023-08-18 09:54:06 +0200

    date picker

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,7 +1,21 @@
+from datetime import datetime
+
 import streamlit as st
 import pandas as pd
 
+from utils.date import get_dates, filter_dates, DATE_TIME_STR
+
 st.title("Pico Temperature")
 
-df = pd.read_csv("/Users/lukasscholz/repositorys/picotemperature/app/data/temperature.csv", names=["Date", "Temperature"], skiprows=[0])
-st.line_chart(data=df, x="Date", y="Temperature")
+df = pd.read_csv("/Users/lukasscholz/repositorys/picotemperature/app/data/temperature.csv",
+                 names=["Date", "Temperature"], skiprows=[0])
+
+with st.sidebar:
+    date_picker = st.date_input("Date", [datetime.strptime(get_dates(df)[1], DATE_TIME_STR).date(),
+                                         datetime.strptime(get_dates(df)[0], DATE_TIME_STR).date()])
+
+try:
+    st.line_chart(data=filter_dates(df, date_picker[0], date_picker[1]), x="Date", y="Temperature")
+except IndexError:
+    st.error("You must pick a start and end date")
+    st.stop()
---
+++ frontend/utils/date.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from pandas import DataFrame
+
+DATE_TIME_STR = "%Y-%m-%d"
+
+
+def get_dates(dataframe: DataFrame):
+    df_dates = dataframe["Date"].tolist()
+    df_dates_lowest = df_dates[0]
+    df_dates_highest = df_dates[-1]
+    return df_dates_lowest, df_dates_highest
+
+
+def filter_dates(dataframe: DataFrame, date_from, date_to):
+    # date_from = datetime.strptime(date_from, DATE_TIME_STR)
+    # date_to = datetime.strptime(date_to, DATE_TIME_STR)
+    return dataframe[(dataframe["Date"] >= date_from.strftime(DATE_TIME_STR)) & (dataframe["Date"] <= date_to.strftime(DATE_TIME_STR))]
