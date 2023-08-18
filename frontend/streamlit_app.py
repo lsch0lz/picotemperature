671ff3b7464dd6002ce37a877ab90768f58cac64
Lukas Scholz <lscholz14@googlemail.com>
2023-08-18 13:47:55 +0200

    added metric fields

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -4,6 +4,8 @@ import streamlit as st
 import pandas as pd
 
 from utils.date import get_dates, filter_dates, DATE_TIME_STR
+from flows.get_metrics import get_metrics
+
 
 st.title("Pico Temperature")
 
@@ -13,9 +15,18 @@ df = pd.read_csv("/Users/lukasscholz/repositorys/picotemperature/app/data/temper
 with st.sidebar:
     date_picker = st.date_input("Date", [datetime.strptime(get_dates(df)[1], DATE_TIME_STR).date(),
                                          datetime.strptime(get_dates(df)[0], DATE_TIME_STR).date()])
-
+    show_metrics = st.checkbox("Show Metrics")
 try:
     st.line_chart(data=filter_dates(df, date_picker[0], date_picker[1]), x="Date", y="Temperature")
+
+    if show_metrics:
+        col1, col2, col3, col4 = st.columns(4)
+        col1.metric(label="Average Temperature", value=f"{get_metrics(filter_dates(df, date_picker[0], date_picker[1]))[0]} \N{DEGREE SIGN}C")
+        col2.metric(label="Median Temperature", value=f"{get_metrics(filter_dates(df, date_picker[0], date_picker[1]))[1]} \N{DEGREE SIGN}C")
+        col3.metric(label="Highest Temperature", value=f"{get_metrics(filter_dates(df, date_picker[0], date_picker[1]))[2]} \N{DEGREE SIGN}C")
+        col4.metric(label="Lowest Temperature", value=f"{get_metrics(filter_dates(df, date_picker[0], date_picker[1]))[3]} \N{DEGREE SIGN}C")
 except IndexError:
     st.error("You must pick a start and end date")
     st.stop()
+
+
